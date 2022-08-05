852143bb1a3e2ffc1d82527f3649f3eaf5840527
Your Name <you@example.com>
2022-08-05 18:29:21 +0200

    correction erreur route

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/commande')]
+#[Route('/commande')]
 class CommandeController extends AbstractController
 {
     #[Route('/', name: 'app_commande_index', methods: ['GET'])]
